15357405a94a2f4bfacf1ec4dd24dd9869463c24
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-02-13 18:18:30 -0500

    First test pass

--- iwal/iwal_functions.py
+++ iwal/iwal_functions.py
@@ -1,3 +1,20 @@
 
-def checkup():
-    return 4
+def append_history(history, x_t, y_t, p_t, q_t):
+    """
+    Adds new sample to given query history.
+
+    :param history:
+    :param x_t:
+    :param y_t:
+    :param p_t:
+    :param q_t:
+    :return:
+    """
+    # validate that history contains required keys
+    if 'X' in history and 'y' in history and 'c' in history and 'Q' in history:
+        history['X'].append(x_t)
+        history['y'].append(y_t)
+        history['c'].append(p_t)
+        history['Q'].append(q_t)
+    else:
+        raise ValueError('history dictionary does not contain the required keys: X,y,c,Q')
--- tests/test_iwal_functions.py
+++ tests/test_iwal_functions.py
@@ -1,7 +1,19 @@
-from iwal.iwal_functions import checkup
+import iwal.iwal_functions
 
 
-def test_checkup():
-    actual = checkup()
-    expected = 4
-    assert(actual, expected)
+def test_append_history_dictionary_matches():
+    h = {
+        'X': [],
+        'y': [],
+        'c': [],
+        'Q': []
+    }
+
+    x_t = [1, 2]
+    y_t = 1
+    p_t = 0.5
+    q_t = 1
+
+    iwal.iwal_functions.append_history(h, x_t, y_t, p_t, q_t)
+
+    # assert x_t in h['X']
