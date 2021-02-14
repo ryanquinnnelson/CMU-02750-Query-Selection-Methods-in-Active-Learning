0075594521c395a79595ea997099d935bdec1097
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-02-14 15:55:49 -0500

    Revised bootstrap process

--- packages/iwal/iwal_functions.py
+++ packages/iwal/iwal_functions.py
@@ -1,10 +1,12 @@
 from scipy.stats import bernoulli
 import numpy as np
+from typing import Any
+from sklearn.metrics import hinge_loss, log_loss
 
 
-def _append_history(history, x_t, y_t, p_t, q_t):
+def _append_history(history: dict, x_t: np.ndarray, y_t: np.ndarray, p_t: float, q_t: int) -> None:
     """
-    Adds new sample to given query history dictionary.
+    Adds given sample to given query history dictionary.
 
     :param history:
     :param x_t:
@@ -21,134 +23,116 @@ def _append_history(history, x_t, y_t, p_t, q_t):
         history['y'].append(y_t)
         history['c'].append(p_t)
         history['Q'].append(q_t)
+
     else:
         raise ValueError('history dictionary does not contain the required keys: X,y,c,Q')
 
-#?? predict_proba for log_loss vs pred_decision for hinge_loss
-def _sum_losses(h, selected, loss, labels):
+
+#
+# #?? predict_proba for log_loss vs pred_decision for hinge_loss
+# def _sum_losses(h, selected, loss, labels):
+#     """
+#     Sums losses over set of labeled elements.
+#
+#     Uses decision_function() for hinge_loss.
+#     Would require predict_proba for log_loss. Need to refactor to allow both.
+#     :param h:
+#     :param selected:
+#     :param loss:
+#     :param labels:
+#     :return:
+#     """
+#     total = 0
+#     for x, y_true, c in selected:
+#         # calculate loss for this sample
+#         y_predict = h.decision_function(x)
+#         curr_loss = loss(y_true, y_predict, labels=labels)
+#         iwal_loss = c * curr_loss
+#
+#         # update total
+#         total += iwal_loss
+#
+#     return total
+#
+#
+# # ?? difference between loss function L() vs l()
+# def _get_min_hypothesis(hypothesis_space, selected, loss, labels):
+#     """
+#     Calculates the minimum hypothesis in the hypothesis space, given a set of labeled samples with weights. Minimum is
+#     defined using the following formula:
+#
+#     h_t = argmin_{h in H} SUM_{x,y,c in S} c * l(h(x),y)
+#
+#     :param hypothesis_space:
+#     :param selected:
+#     :param loss:
+#     :param labels:
+#     :return:
+#     """
+#     min_loss = 10000
+#     min_h = None
+#
+#     # consider each model in hypothesis space
+#     for i in range(len(hypothesis_space)):
+#
+#         # sum losses over all labeled elements
+#         h = hypothesis_space[i]
+#         curr_loss = _sum_losses(h, selected, loss, labels)
+#
+#         # update minimum loss
+#         if curr_loss < min_loss:
+#             min_loss = curr_loss
+#             min_h = hypothesis_space[i]
+#
+#     return min_h
+#
+# def iwal_query(x_t, y_t, hypothesis_space, history, selected, rejection_threshold):
+#     pass
+#
+#
+
+
+def _loss_difference_hinge_loss(y_true: np.ndarray, pred_decision_a: np.ndarray, pred_decision_b: np.ndarray,
+                                labels: list) -> float:
     """
-    Sums losses over set of labeled elements.
-
-    Uses decision_function() for hinge_loss.
-    Would require predict_proba for log_loss. Need to refactor to allow both.
-    :param h:
-    :param selected:
-    :param loss:
-    :param labels:
-    :return:
-    """
-    total = 0
-    for x, y_true, c in selected:
-        # calculate loss for this sample
-        y_predict = h.decision_function(x)
-        curr_loss = loss(y_true, y_predict, labels=labels)
-        iwal_loss = c * curr_loss
-
-        # update total
-        total += iwal_loss
-
-    return total
-
-
-# ?? difference between loss function L() vs l()
-def _get_min_hypothesis(hypothesis_space, selected, loss, labels):
-    """
-    Calculates the minimum hypothesis in the hypothesis space, given a set of labeled samples with weights. Minimum is
-    defined using the following formula:
-
-    h_t = argmin_{h in H} SUM_{x,y,c in S} c * l(h(x),y)
-
-    :param hypothesis_space:
-    :param selected:
-    :param loss:
-    :param labels:
-    :return:
-    """
-    min_loss = 10000
-    min_h = None
-
-    # consider each model in hypothesis space
-    for i in range(len(hypothesis_space)):
-
-        # sum losses over all labeled elements
-        h = hypothesis_space[i]
-        curr_loss = _sum_losses(h, selected, loss, labels)
-
-        # update minimum loss
-        if curr_loss < min_loss:
-            min_loss = curr_loss
-            min_h = hypothesis_space[i]
-
-    return min_h
-
-
-# ?? how to choose Q_t? use Bernoulli distribution?
-# ?? no y_t in parameters but listed in documentation
-# ?? no S in parameters but is in paper
-# ?? confirm h is list of models? Algorithm 1 requires argmin over all h in H...
-def iwal_query(x_t, y_t, selected, rejection_threshold, history, hypothesis_space, loss, labels, **additional):
-    """
-    This function implements Algorithm 1 IWAL (subroutine rejection-threshold) from the paper by Beygelzimer et al. See
-    https://arxiv.org/pdf/0812.4952.pdf.
-
-    :param loss: Python function which calculates loss for a given hypothesis.
-    :param labels: List of possible labels for the data set.
-    :param x_t: Sample currently being considered for labelling.
-    :param y_t: True label for sample.
-    :param selected: List representing samples chosen for labeling. Each element in the list is a tuple (x,y,c), where
-    x is the data, y is the label, and c is 1/p_t, where p_t is the rejection threshold probability for this sample.
-    :param rejection_threshold: Python function which calculates the rejection threshold probability.
-    :param history: Dictionary of query history. Must contain the keys 'X', 'y', 'p', 'Q'. The value of each key is a
-    List containing the following:
-            X -- sampled data points
-            y -- labels matching to data points
-            p -- rejection probabilities matching to data points
-            Q -- coin flips matching to data points
-    :param hypothesis_space: A list of scikit-learn models (i.e. sklearn.linear_model.LogisticRegression)
-    :param additional: Dictionary of arbitrary arguments that may be required by rejection_threshold().
-    :return: Instance (scikit-learn model) of hypothesis space that is optimal at current time step.
-    """
-
-    # derive probability of requesting label for x_t
-    if additional:
-        p_t = rejection_threshold(x_t, history, additional['kwargs'])  # pass additional arguments
-    else:
-        p_t = rejection_threshold(x_t, history)
-
-    # flip a coin using derived probability
-    Q_t = bernoulli.rvs(p_t)
-
-    # save query in history
-    _append_history(history, x_t, y_t, p_t, Q_t)
-
-    # choose actions based on flip
-    if Q_t == 1:  # label is requested
-        c_t = 1.0 / p_t
-        selected.append((x_t, y_t, c_t))  # add to set of selected samples
-
-    # select model with least loss
-    h_t = _get_min_hypothesis(hypothesis_space, selected, loss, labels)
-
-    return h_t
-
-
-def _loss_difference(y_true, y_pred_i, y_pred_j, loss, labels):
-    """
-    Calculates the difference in loss between two predictions.
+    Calculates the difference in loss between a pair of prediction decisions, using hinge_loss.
 
     :param y_true:
-    :param y_pred_i:
-    :param y_pred_j:
+    :param pred_decision_a:
+    :param pred_decision_b:
     :param labels:
-    :param loss:
     :return:
     """
-    loss_i = loss(y_true, y_pred_i, labels=labels)
-    loss_j = loss(y_true, y_pred_j, labels=labels)
-    return loss_i - loss_j
+    loss_a = hinge_loss(y_true, pred_decision_a, labels=labels)
+    loss_b = hinge_loss(y_true, pred_decision_b, labels=labels)
+
+    return loss_a - loss_b
 
 
-def _bootstrap_probability(p_min, max_loss_difference):
+def _loss_difference(y_true: np.ndarray, y_pred_a: np.ndarray, y_pred_b: np.ndarray, labels: list,
+                     loss_function: str) -> float:
+    """
+    Calculates the difference in loss between a pair of prediction decisions, using given loss_function.
+
+    :param y_true:
+    :param y_pred_a:
+    :param y_pred_b:
+    :param labels:
+    :param loss_function:
+    :return:
+    """
+    print('_loss_difference()')
+    print(y_true)
+    print(y_pred_a)
+    print(y_pred_b)
+
+    if loss_function == 'hinge_loss':
+        return _loss_difference_hinge_loss(y_true, y_pred_a, y_pred_b, labels)
+    else:
+        raise NotImplementedError('Function does not support loss_function: ' + loss_function)
+
+
+def _bootstrap_probability(p_min: float, max_loss_difference: float) -> float:
     """
     Calculates the rejection threshold probability using the following formula:
 
@@ -161,10 +145,28 @@ def _bootstrap_probability(p_min, max_loss_difference):
     return p_min + (1 - p_min) * max_loss_difference
 
 
-# ?? don't use loss parameter defined in the parameters
+def _get_prediction(x: np.ndarray, h: Any, loss_function: str) -> np.ndarray:
+    """
+    Calculates the appropriate prediction, given the loss_function.
+
+    :param x:
+    :param h:
+    :param loss_function:
+    :return:
+    """
+
+    if loss_function == 'hinge_loss':
+        return h.decision_function(x)
+    elif loss_function == 'log_loss':
+        return h.predict_proba(x)
+    else:
+        raise NotImplementedError('Function does not support loss_function: ' + loss_function)
+
+
+# ?? what loss function to use? don't use loss parameter defined in the parameters
 # ?? what to return if hypothesis space or labels are empty
 # ?? how to know it is working correctly
-def bootstrap(x, history, additional_args):
+def _bootstrap(x: np.ndarray, hypothesis_space: list, labels: list, p_min: float, loss_function: str) -> float:
     """
     This function implements the bootstrap rejection threshold subroutine defined in 7.2. Bootstrap instantiation of
     IWAL from the paper by Beygelzimer et al. See https://arxiv.org/pdf/0812.4952.pdf. Calculates rejection threshold
@@ -172,36 +174,117 @@ def bootstrap(x, history, additional_args):
 
     p_t = p_min + (1 - p_min) * {max_{y, h_i, h_j} L(h_i(x), y) - L(h_j(x), y)}
 
-    :param history:
     :param x:
-    :param additional_args:
+    :param hypothesis_space:
+    :param labels:
+    :param p_min:
+    :param loss_function:
     :return:
     """
-    # set additional arguments
-    hypothesis_space = additional_args['H']
-    loss = additional_args['loss']
-    labels = additional_args['labels']
-    p_min = additional_args['p_min']
-
-    # find max loss difference
-    max_loss_difference = -10000
+    max_diff = -10000
 
     # consider every pair of models in the hypothesis space combined with every label
     for i in range(len(hypothesis_space)):
         for j in range(len(hypothesis_space)):
             for label in labels:
 
+                # based on loss_function, get appropriate prediction to calculate loss
+                h_i = hypothesis_space[i]
+                h_j = hypothesis_space[j]
+                print(i,j,label)
+                print(type(h_i))
+                print(type(h_j))
+
+                y_pred_i = _get_prediction(x, h_i, loss_function)
+                y_pred_j = _get_prediction(x, h_j, loss_function)
+                print(y_pred_i)
+                print(y_pred_j)
+
                 # calculate loss difference between models
                 y_true = np.full(shape=len(x), fill_value=label, dtype=np.int)
-                y_pred_i = hypothesis_space[i].predict(x)
-                y_pred_j = hypothesis_space[j].predict(x)
-
-                curr_difference = _loss_difference(y_true, y_pred_i, y_pred_j, loss, labels)
-
+                diff = _loss_difference(y_true, y_pred_i, y_pred_j, labels, loss_function)
+                print(diff)
                 # update max
-                if curr_difference > max_loss_difference:
-                    max_loss_difference = curr_difference
+                if diff > max_diff:
+                    max_diff = diff
 
-    # calculate p_t
-    p_t = _bootstrap_probability(p_min, max_loss_difference)
+    # calculate rejection threshold probability
+    p_t = _bootstrap_probability(p_min, max_diff)
     return p_t
+
+
+#
+# # ?? how to choose Q_t? use Bernoulli distribution?
+# # ?? no y_t in parameters but listed in documentation
+# # ?? no S in parameters but is in paper
+# # ?? confirm h is list of models? Algorithm 1 requires argmin over all h in H...
+# def iwal_query_bootstrap(x_t:np.ndarray, y_t:np.ndarray,  hypothesis_space: list, history: dict, selected: list, loss: Any, labels: list, p_min: float=0.1 ) -> Any:
+#     """
+#
+#     :param x_t:
+#     :param y_t:
+#     :param hypothesis_space:
+#     :param history:
+#     :param selected:
+#     :param loss:
+#     :param labels:
+#     :return:
+#     """
+#     # derive probability of requesting label for x_t
+#     p_t = None
+
+
+# def iwal_query(x_t, y_t, selected, rejection_threshold, history, hypothesis_space, loss, labels, **additional):
+#     """
+#     This function implements Algorithm 1 IWAL (subroutine rejection-threshold) from the paper by Beygelzimer et al. See
+#     https://arxiv.org/pdf/0812.4952.pdf.
+#
+#     :param loss: Python function which calculates loss for a given hypothesis.
+#     :param labels: List of possible labels for the data set.
+#     :param x_t: Sample currently being considered for labelling.
+#     :param y_t: True label for sample.
+#     :param selected: List representing samples chosen for labeling. Each element in the list is a tuple (x,y,c), where
+#     x is the data, y is the label, and c is 1/p_t, where p_t is the rejection threshold probability for this sample.
+#     :param rejection_threshold: Python function which calculates the rejection threshold probability.
+#     :param history: Dictionary of query history. Must contain the keys 'X', 'y', 'p', 'Q'. The value of each key is a
+#     List containing the following:
+#             X -- sampled data points
+#             y -- labels matching to data points
+#             p -- rejection probabilities matching to data points
+#             Q -- coin flips matching to data points
+#     :param hypothesis_space: A list of scikit-learn models (i.e. sklearn.linear_model.LogisticRegression)
+#     :param additional: Dictionary of arbitrary arguments that may be required by rejection_threshold().
+#     :return: Instance (scikit-learn model) of hypothesis space that is optimal at current time step.
+#     """
+#
+#
+#     if additional:
+#         p_t = rejection_threshold(x_t, history, additional['kwargs'])  # pass additional arguments
+#     else:
+#         p_t = rejection_threshold(x_t, history)
+#
+#     # flip a coin using derived probability
+#     Q_t = bernoulli.rvs(p_t)
+#
+#     # save query in history
+#     _append_history(history, x_t, y_t, p_t, Q_t)
+#
+#     # choose actions based on flip
+#     if Q_t == 1:  # label is requested
+#         c_t = 1.0 / p_t
+#         selected.append((x_t, y_t, c_t))  # add to set of selected samples
+#
+#     # select model with least loss
+#     h_t = _get_min_hypothesis(hypothesis_space, selected, loss, labels)
+#
+#     return h_t
+#
+#
+
+#
+#
+
+#
+#
+
+
--- tests/test_iwal_functions.py
+++ tests/test_iwal_functions.py
@@ -2,6 +2,8 @@ import packages.iwal.iwal_functions
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import hinge_loss, log_loss
 import numpy as np
+import pytest
+from sklearn import svm
 
 
 def test_append_history_dictionary_matches():
@@ -24,169 +26,227 @@ def test_append_history_dictionary_matches():
     assert p_t in history['c']
     assert q_t in history['Q']
 
+#
+# def test__get_min_hypothesis_empty_hypothesis_space():
+#     hypothesis_space = []
+#     s = {(1, 1, 0.5)}
+#     labels = [0, 1]
+#
+#     min_h = packages.iwal.iwal_functions._get_min_hypothesis(hypothesis_space, s, log_loss, labels)
+#     if min_h is None:
+#         assert True
+#     else:
+#         assert False
+#
+#
+# def test__get_min_hypothesis_empty_set():
+#     # example data set
+#     X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
+#     y = [1, 0]
+#
+#     lr = LogisticRegression().fit(X, y)
+#     hypothesis_space = [lr]
+#     s = set()
+#     labels = [0, 1]
+#
+#     min_h = packages.iwal.iwal_functions._get_min_hypothesis(hypothesis_space, s, log_loss, labels)
+#     if min_h is lr:
+#         assert True
+#     else:
+#         assert False
+#
+#
+# def test__get_min_hypothesis_one_hypothesis():
+#     # example data set
+#     X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
+#     y = [1, 0]
+#     lr = LogisticRegression().fit(X, y)
+#     hypothesis_space = [lr]
+#
+#     # example labeled set
+#     x_t = [[3, 1]]
+#     y_t = [1]
+#     c_t = 0.1
+#     s = [(x_t, y_t, c_t)]
+#
+#     labels_t = [0, 1]
+#
+#     min_h = packages.iwal.iwal_functions._get_min_hypothesis(hypothesis_space, s, log_loss, labels_t)
+#     if min_h is lr:
+#         assert True
+#     else:
+#         assert False
+#
+#
+# def test__sum_losses():
+#     # example data set
+#     X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
+#     y = [1, 0]
+#     lr = LogisticRegression().fit(X, y)
+#
+#     # example labeled set
+#     x_t = np.asarray([3, 1]).reshape(1, -1)  # single element with 2 features
+#     y_t = np.asarray([1])
+#     c_t = 0.1
+#     s = [(x_t, y_t, c_t)]
+#
+#     labels_t = [0, 1]
+#
+#     # define dummy loss function for testing purposes
+#     def loss_func(a, b, labels):
+#         return a
+#
+#     total = packages.iwal.iwal_functions._sum_losses(lr, s, loss_func, labels_t)
+#     assert total == 0.1
+#
+#
+# def test_iwal_query_selected_for_labeling():
+#     # example data set
+#     X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
+#     y = [1, 0]
+#     lr = LogisticRegression().fit(X, y)
+#     hypothesis_space = [lr]
+#
+#     # example labeled set
+#     x_t = np.asarray([3, 1]).reshape(1, -1)  # single element with 2 features
+#     y_t = np.asarray([1])
+#     selected = []
+#
+#     # dummy function for testing
+#     def rejection_func(x_t, history):
+#         return 1.0
+#
+#     # define dummy loss function for testing purposes
+#     def loss_func(a, b, labels):
+#         l = labels
+#         return a
+#
+#     history = {
+#         'X': [],
+#         'y': [],
+#         'c': [],
+#         'Q': []
+#     }
+#
+#     labels_t = [0, 1]
+#
+#     h_t = packages.iwal.iwal_functions.iwal_query(x_t, y_t, selected, rejection_func, history, hypothesis_space,
+#                                                   loss_func, labels_t)
+#
+#     assert len(selected) == 1
+#     if h_t is lr:
+#         assert True
+#     else:
+#         assert False
+#
+#
+# def test_iwal_query_not_selected_for_labeling():
+#     # example data set
+#     X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
+#     y = [1, 0]
+#     lr = LogisticRegression().fit(X, y)
+#     hypothesis_space = [lr]
+#
+#     # example labeled set
+#     x_t = np.asarray([3, 1]).reshape(1, -1)  # single element with 2 features
+#     y_t = np.asarray([1])
+#     selected = []
+#
+#     # dummy function for testing
+#     def rejection_func(x_t, history):
+#         return 0.0
+#
+#     # define dummy loss function for testing purposes
+#     def loss_func(a, b, labels):
+#         l = labels
+#         return a + b
+#
+#     history = {
+#         'X': [],
+#         'y': [],
+#         'c': [],
+#         'Q': []
+#     }
+#
+#     labels_t = [0, 1]
+#
+#     h_t = packages.iwal.iwal_functions.iwal_query(x_t, y_t, selected, rejection_func, history, hypothesis_space,
+#                                                   loss_func, labels_t)
+#
+#     assert len(selected) == 0
+#     if h_t is lr:
+#         assert True
+#     else:
+#         assert False
+#
+#
+def test__loss_difference_hinge_loss():
 
-def test__get_min_hypothesis_empty_hypothesis_space():
-    hypothesis_space = []
-    s = {(1, 1, 0.5)}
+    X = [[0], [1]]
+    y = [-1, 1]
+    est1 = svm.LinearSVC(random_state=0)
+    est1.fit(X, y)
+    pred_decision1 = est1.decision_function([[-2], [3], [0.5]])
+    loss1 = hinge_loss([-1, 1, 1], pred_decision1)
+
+    est2 = LogisticRegression()
+    est2.fit(X, y)
+    pred_decision2 = est2.decision_function([[-2], [3], [0.5]])
+    loss2 = hinge_loss([-1, 1, 1], pred_decision2)
+
+    test_y_true = np.asarray([-1, 1, 1])
     labels = [0, 1]
 
-    min_h = packages.iwal.iwal_functions._get_min_hypothesis(hypothesis_space, s, log_loss, labels)
-    if min_h is None:
-        assert True
-    else:
-        assert False
+    expected = loss1 - loss2
+
+    actual = packages.iwal.iwal_functions._loss_difference_hinge_loss(test_y_true,pred_decision1,pred_decision2,labels)
+    assert actual == expected
 
 
-def test__get_min_hypothesis_empty_set():
-    # example data set
-    X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
-    y = [1, 0]
+def test__loss_difference_implemented_loss_function():
 
-    lr = LogisticRegression().fit(X, y)
-    hypothesis_space = [lr]
-    s = set()
+    X = [[0], [1]]
+    y = [-1, 1]
+    est1 = svm.LinearSVC(random_state=0)
+    est1.fit(X, y)
+    pred_decision1 = est1.decision_function([[-2], [3], [0.5]])
+    loss1 = hinge_loss([-1, 1, 1], pred_decision1)
+
+    est2 = LogisticRegression()
+    est2.fit(X, y)
+    pred_decision2 = est2.decision_function([[-2], [3], [0.5]])
+    loss2 = hinge_loss([-1, 1, 1], pred_decision2)
+
+    test_y_true = np.asarray([-1, 1, 1])
     labels = [0, 1]
+    loss_function = 'hinge_loss'
 
-    min_h = packages.iwal.iwal_functions._get_min_hypothesis(hypothesis_space, s, log_loss, labels)
-    if min_h is lr:
-        assert True
-    else:
-        assert False
+    expected = loss1 - loss2
 
-
-def test__get_min_hypothesis_one_hypothesis():
-    # example data set
-    X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
-    y = [1, 0]
-    lr = LogisticRegression().fit(X, y)
-    hypothesis_space = [lr]
-
-    # example labeled set
-    x_t = [[3, 1]]
-    y_t = [1]
-    c_t = 0.1
-    s = [(x_t, y_t, c_t)]
-
-    labels_t = [0, 1]
-
-    min_h = packages.iwal.iwal_functions._get_min_hypothesis(hypothesis_space, s, log_loss, labels_t)
-    if min_h is lr:
-        assert True
-    else:
-        assert False
+    actual = packages.iwal.iwal_functions._loss_difference(test_y_true, pred_decision1, pred_decision2,
+                                                                      labels, loss_function)
+    assert actual == expected
 
 
-def test__sum_losses():
-    # example data set
-    X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
-    y = [1, 0]
-    lr = LogisticRegression().fit(X, y)
+def test__loss_difference_not_implemented_loss_function():
 
-    # example labeled set
-    x_t = np.asarray([3, 1]).reshape(1, -1)  # single element with 2 features
-    y_t = np.asarray([1])
-    c_t = 0.1
-    s = [(x_t, y_t, c_t)]
+    X = [[0], [1]]
+    y = [-1, 1]
+    est1 = svm.LinearSVC(random_state=0)
+    est1.fit(X, y)
+    pred_decision1 = est1.decision_function([[-2], [3], [0.5]])
 
-    labels_t = [0, 1]
+    est2 = LogisticRegression()
+    est2.fit(X, y)
+    pred_decision2 = est2.decision_function([[-2], [3], [0.5]])
 
-    # define dummy loss function for testing purposes
-    def loss_func(a, b, labels):
-        return a
-
-    total = packages.iwal.iwal_functions._sum_losses(lr, s, loss_func, labels_t)
-    assert total == 0.1
-
-
-def test_iwal_query_selected_for_labeling():
-    # example data set
-    X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
-    y = [1, 0]
-    lr = LogisticRegression().fit(X, y)
-    hypothesis_space = [lr]
-
-    # example labeled set
-    x_t = np.asarray([3, 1]).reshape(1, -1)  # single element with 2 features
-    y_t = np.asarray([1])
-    selected = []
-
-    # dummy function for testing
-    def rejection_func(x_t, history):
-        return 1.0
-
-    # define dummy loss function for testing purposes
-    def loss_func(a, b, labels):
-        l = labels
-        return a
-
-    history = {
-        'X': [],
-        'y': [],
-        'c': [],
-        'Q': []
-    }
-
-    labels_t = [0, 1]
-
-    h_t = packages.iwal.iwal_functions.iwal_query(x_t, y_t, selected, rejection_func, history, hypothesis_space,
-                                                  loss_func, labels_t)
-
-    assert len(selected) == 1
-    if h_t is lr:
-        assert True
-    else:
-        assert False
-
-
-def test_iwal_query_not_selected_for_labeling():
-    # example data set
-    X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
-    y = [1, 0]
-    lr = LogisticRegression().fit(X, y)
-    hypothesis_space = [lr]
-
-    # example labeled set
-    x_t = np.asarray([3, 1]).reshape(1, -1)  # single element with 2 features
-    y_t = np.asarray([1])
-    selected = []
-
-    # dummy function for testing
-    def rejection_func(x_t, history):
-        return 0.0
-
-    # define dummy loss function for testing purposes
-    def loss_func(a, b, labels):
-        l = labels
-        return a + b
-
-    history = {
-        'X': [],
-        'y': [],
-        'c': [],
-        'Q': []
-    }
-
-    labels_t = [0, 1]
-
-    h_t = packages.iwal.iwal_functions.iwal_query(x_t, y_t, selected, rejection_func, history, hypothesis_space,
-                                                  loss_func, labels_t)
-
-    assert len(selected) == 0
-    if h_t is lr:
-        assert True
-    else:
-        assert False
-
-
-def test__loss_difference():
-    test_y_true = np.asarray([0, 1])
-    test_y_pred_i = np.asarray([1, 1])
-    test_y_pred_j = np.asarray([0, 1])
+    test_y_true = np.asarray([-1, 1, 1])
     labels = [0, 1]
+    loss_function = 'log_loss'
 
-    actual = packages.iwal.iwal_functions._loss_difference(test_y_true, test_y_pred_i, test_y_pred_j, hinge_loss,
-                                                           labels)
-    assert actual == 0.5
+    with pytest.raises(NotImplementedError):
+        packages.iwal.iwal_functions._loss_difference(test_y_true, pred_decision1, pred_decision2,
+                                                                      labels, loss_function)
 
 
 def test__bootstrap_probability():
@@ -196,23 +256,57 @@ def test__bootstrap_probability():
     assert actual == 0.55
 
 
-def test_bootstrap_one_hypothesis():
-    x = [[3, 1]]
+def test__get_prediction_hinge_loss():
 
-    # example data set
-    X = [[2.59193175, 1.14706863], [1.7756532, 1.15670278]]
-    y = [1, 0]
-    lr = LogisticRegression().fit(X, y)
-    hypothesis_space = [lr]
-    p_min = 0.1
+    X = [[0], [1]]
+    y = [-1, 1]
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    x = [[-2]]
+    pred_decision = lr.decision_function(x)
+
+    assert pred_decision == packages.iwal.iwal_functions._get_prediction(x, lr, 'hinge_loss')
+
+
+def test__get_prediction_log_loss():
+
+    X = [[0], [1]]
+    y = [-1, 1]
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    x = [[-2]]
+    expected = lr.predict_proba(x).tolist()
+    actual = packages.iwal.iwal_functions._get_prediction(x, lr, 'log_loss').tolist()
+
+    assert expected == actual
+
+
+def test__get_prediction_not_implemented():
+
+    X = [[0], [1]]
+    y = [-1, 1]
+    lr = LogisticRegression()
+    lr.fit(X, y)
+    x = [[-2]]
+
+    with pytest.raises(NotImplementedError):
+        packages.iwal.iwal_functions._get_prediction(x, lr, 'other')
+
+
+def test_bootstrap_two_hypotheses():
+    X = [[0], [1]]
+    y = [-1, 1]
+    sv = svm.LinearSVC(random_state=0)
+    sv.fit(X, y)
+
+    lr = LogisticRegression()
+    lr.fit(X, y)
+
+    H = [sv,lr]
+    x= [[-2]]
     labels = [0, 1]
-    history = dict()
-    additional = {
-        'H': hypothesis_space,
-        'loss': hinge_loss,
-        'labels': labels,
-        'p_min': p_min
-    }
+    loss_function = 'hinge_loss'
+    p_min = 0.1
 
-    actual = packages.iwal.iwal_functions.bootstrap(x, history, additional)
-    assert actual == p_min
+    packages.iwal.iwal_functions._bootstrap(x, H, labels, p_min,loss_function)
+
