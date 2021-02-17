b9fb34b0b46368e61f697ef8c20448e04031afc9
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-02-17 14:29:46 -0500

    Reformat code

--- packages/iwal/helper.py
+++ packages/iwal/helper.py
@@ -1,7 +1,7 @@
 """
 Defines common functions shared throughout package.
 """
-from sklearn.metrics import hinge_loss,log_loss
+from sklearn.metrics import hinge_loss, log_loss
 import numpy as np
 
 
@@ -18,5 +18,5 @@ def calculate_hinge_loss(x, h, label, labels):
 def calculate_log_loss(x, h, label, labels):
     y_true = np.full(shape=len(x), fill_value=label, dtype=np.int)
     y_pred = h.predict_proba(x)
-    loss = log_loss(y_true,y_pred,labels=labels)
+    loss = log_loss(y_true, y_pred, labels=labels)
     return loss
